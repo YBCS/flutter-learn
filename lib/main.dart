48858c361f1012564013a2b49ca36145652e03d3
YBCS <budha627@gmail.com>
2019-11-15 00:19:00 +0530

    upto #6 fonts and color -- fonts yet to be done

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('my app is here '),
+          centerTitle: true,
+          backgroundColor: Colors.red[600],
+        ),
+
+        body: Center(
+          child: Text(
+            'hello ninjas',
+            style: TextStyle(
+              // this are the styles
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+              color: Colors.grey[600],
+              // note : can also add custom font family
+            ),
+          ),
+        ),
+
+        floatingActionButton: FloatingActionButton(
+          child: Text('click'),
+          onPressed: () {},   /*anonimous function*/
+          backgroundColor: Colors.red[600],
+        ),
+
+      ),
+
+    ));
